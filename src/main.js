8e5bb5cd761ee47718977b5da6c55cc0a30ab965
Chen Frederick <frederickfd88@gmail.com>
2022-04-08 22:38:42 +0700

    Added "VueHtml2Canvas" Plugin

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import VueHtml2Canvas from 'vue-html2canvas';
+import "regenerator-runtime/runtime.js";
+
+Vue.use(VueHtml2Canvas);
 Vue.config.productionTip = false
 
 new Vue({
