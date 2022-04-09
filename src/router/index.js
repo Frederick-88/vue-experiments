749a1f82eeecb2391d49e0518031b616bf14c632
Chen Frederick <frederickfd88@gmail.com>
2022-04-09 22:05:50 +0700

    Added Routes for Experiments on Vue App

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,63 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
-  }
-]
+    component: function() {
+      return import(/* webpackChunkName: "about" */ "../views/About.vue");
+    },
+  },
+  {
+    path: "/glitch-image",
+    name: "GlitchImage",
+    component: function() {
+      return import(
+        /* webpackChunkName: "GlitchImage" */ "../views/GlitchImage.vue"
+      );
+    },
+  },
+  {
+    path: "/mock-progress-bar",
+    name: "MockProgressBar",
+    component: function() {
+      return import(
+        /* webpackChunkName: "MockProgressBar" */ "../views/MockProgressBar.vue"
+      );
+    },
+  },
+  {
+    path: "/video-captureframe",
+    name: "VideoCaptureFrame",
+    component: function() {
+      return import(
+        /* webpackChunkName: "VideoCaptureFrame" */ "../views/VideoCaptureFrame.vue"
+      );
+    },
+  },
+  {
+    path: "/tweenmax",
+    name: "Tweenmax",
+    component: function() {
+      return import(/* webpackChunkName: "Tweenmax" */ "../views/Tweenmax.vue");
+    },
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
